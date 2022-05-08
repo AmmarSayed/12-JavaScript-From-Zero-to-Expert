8f8ddb7f079ff27693043f4665dcdfba603ba057
Ammar Sayed <ammarsayed1988@gmail.com>
2022-05-08 12:25:41 +0200

    end of lecture 10

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+if (js === 'amazing') console.log('JavaScript is Fun!');
+console.log(40 + 8 + 23 - 10);
+
+let firstName = 'Ammar';
+let $function = 27;
+
+let person = 'Ammar';
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJon = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
