66a07e971b6f94023e07e1050604c3918593d86c
Ammar Sayed <ammarsayed1988@gmail.com>
2022-05-09 18:09:46 +0200

    end of challange#2

--- 01-Fundamentals-Part-1/starter/challange01.js
+++ 01-Fundamentals-Part-1/starter/challange01.js
@@ -23,7 +23,7 @@ const johnMass = 92;
 const johnHeight = 1.95;
 
 const markBMI = markMass / markHeight ** 2;
-const johnBMI = johnHeight / johnHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
 
 const markHigherBMI = markBMI > johnBMI;
 
